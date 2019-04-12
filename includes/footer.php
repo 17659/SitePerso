8af5c25b7fb43f948f85d11ba330f7e012d91b89
Exposito Esteban <17659@csmrouen.net>
2019-04-12 09:00:13 +0200

    Fail de la tentative de faire du PHP, je comprends rien à ce que je fait ....

---
+++ includes/footer.php
@@ -0,0 +1,42 @@
+<?php
+<!-- Footer -->
+<footer id="footer">
+    <div class="inner">
+        <div class="content">
+            <section>
+                <h3>Informations concernant ce site</h3>
+                <p>Ce site web est un projet dans le cadre de la formation "Référent Digital" de la <a href="https://nfactory.school/">NFactory School</a> au
+                    campus Saint Marc de Rouen.</p>
+            </section>
+            <section>
+                <h4>Musique</h4>
+                <ul class="alt">
+                    <li><a href="https://www.youtube.com/watch?v=U3FtUERy25g">Black Rebel Motorcycle Club - Devil's
+                        Waitin'</a></li>
+                </ul>
+            </section>
+            <section>
+                <h4>Liens Linkedin et Github</h4>
+                <ul class="plain">
+                    <li><a href="https://www.linkedin.com/in/esteban-exposito-522727180/"><i class="icon fa-linkedin">&nbsp;</i>Linkedin</a>
+                    </li>
+                    <li><a href="https://github.com/17659"><i class="icon fa-github">&nbsp;</i>Github</a></li>
+                </ul>
+            </section>
+        </div>
+        <div class="copyright">
+            &copy; Exposito Esteban.
+        </div>
+    </div>
+</footer>
+
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/browser.min.js"></script>
+<script src="assets/js/breakpoints.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,243 @@
+<body class="is-preload">
+
+<!-- Header -->
+<header id="header">
+    <a class="logo" href="index.html">Esteban Exposito</a>
+    <audio controls preload="metadata">
+        <source src="musiques/Black Rebel Motorcycle Club.mp">
+        <source src="musiques/Black Rebel Motorcycle Club.ogg">
+        Veuillez mettre à jour votre navigateur !
+    </audio>
+    <nav>
+        <a href="#menu">Menu</a>
+    </nav>
+</header>
+
+<!-- Nav -->
+<nav id="menu">
+    <ul class="links">
+        <li><a href="index.html">Accueil</a></li>
+        <li><a href="https://www.linkedin.com/in/esteban-exposito-522727180/">Linkedin</a></li>
+        <li><a href="https://github.com/17659"">GitHub</a></li>
+
+        <form method="post" action="/">
+            <div>
+                <label for="nom">Nom : </label><input type="text" id="nom" name="nom" placeholder="Saisir votre nom"/>
+            </div>
+            <div>
+                <label for="prenom">Prénom : </label><input type="text" id="prenom" name="prenom"
+                                                            placeholder="Saisir votre prenom"/>
+            </div>
+            <div>
+                <label for="mail">Mail : </label><input type="text" id="mail" name="mail"
+                                                        placeholder="Saisir votre mail"/>
+            </div>
+            <div>
+                <label for="tel">Telephone : </label><input type="text" id="tel" name="tel"
+                                                            placeholder="Saisir votre numéro"/>
+            </div>
+            <div>
+                <label for="message">Saisir votre message:</label><textarea id="message" name="message" rows="5" cols="33"
+                                                                            placeholder="Saisir votre message"></textarea>
+
+            </div>
+            <input type="reset" value="Effacer"/>
+            <input type="submit" value="Envoyer"/>
+            <input type="hidden" name="gollum" />
+        </form>
+    </ul>
+</nav>
+
+<!-- Banner -->
+<section id="banner">
+    <div class="inner">
+        <h1>Bonjour</h1>
+        <p>Voici mon site personnel, vous y trouverez mon curriculum vitæ, un petit article retraçant mon
+            parcours professionnel et quelques témoignages. Vous pouvez aussi me contacter via le formulaire de contact qui se situe dans le menu, ainsi que les liens vers mon Linkedin et mon Github.</p>
+    </div>
+    <video autoplay loop muted playsinline src="images/banner.mp4"></video>
+</section>
+
+<!-- Highlights -->
+<section class="wrapper">
+    <div class="inner">
+        <header class="special">
+            <h2>Curriculum vitæ</h2>
+            <h3>Référent Digital Junior</h3>
+        </header>
+        <div class="highlights">
+            <section>
+                <div class="content">
+                    <header>
+                        <a href="#" class="icon fa-vcard-o"><span class="label">Icon</span></a>
+                        <h3><strong>Informations</strong></h3>
+                    </header>
+                    <p><strong>Esteban Exposito</strong></p>
+                    <p><strong>Exposito.Esteban@outlook.fr</strong></p>
+                    <p><strong>27 Rue Jacques Boutrolle 76130 Mont Saint Aignan</strong></p>
+                    <p><strong>25 ans</strong></p>
+                    <p><strong>06 52 79 59 06</strong></p>
+                    <p><strong>Travailleur handicapé</strong></p>
+                </div>
+            </section>
+            <section>
+                <div class="content">
+                    <header>
+                        <a href="#" class="icon fa-qrcode"><span class="label">Icon</span></a>
+                        <h3><strong>Atouts</strong></h3>
+                    </header>
+                    <p><strong>Polyvalent</strong></p>
+                    <p><strong>Autonome</strong></p>
+                    <p><strong>Responsable</strong></p>
+                    <p><strong>Fiable</strong></p>
+                </div>
+            </section>
+            <section>
+                <div class="content">
+                    <header>
+                        <a href="#" class="icon fa-files-o"><span class="label">Icon</span></a>
+                        <h3><strong>Compéténces</strong></h3>
+                    </header>
+                    <p><strong>Management</strong> (Gestion d'apprentis et de stagiaires)</p>
+                    <p><strong>Gestion</strong> (Accueil de marchandise, vérification et stockage)</p>
+                    <p><strong>Administrer et gérer un site sous Wordpress</strong></p>
+                    <p><strong>Analyser les besoins du client</strong></p>
+                    <p><strong>Communiquer et prospecter via des outils en ligne</strong></p>
+                </div>
+            </section>
+            <section>
+                <div class="content">
+                    <header>
+                        <a href="#" class="icon fa-floppy-o"><span class="label">Icon</span></a>
+                        <h3><strong>Informatique</strong></h3>
+                    </header>
+                    <p><strong>Retouche Photo (Débutant)</strong>
+                        Photophiltre / Adobe Photoshop</p>
+                    <p><strong>Montage Vidéo (Débutant)</strong>
+                        Adobe Premiere Pro</p>
+                    <p><strong>Developpement (Débutant)</strong>
+                        Langage Web (HTML, CSS, JavaScript, PHP, SQL) / Atome / PHPStorm / GitHub</p>
+                </div>
+            </section>
+            <section>
+                <div class="content">
+                    <header>
+                        <a href="#" class="icon fa-paper-plane-o"><span class="label">Icon</span></a>
+                        <h3><strong>Diplômes et Formations</strong></h3>
+                    </header>
+                    <p><strong>De janvier 2019 à mai 2019 :</strong> Référent Digital NFactory School Rouen, France</p>
+                    <p><strong>De juillet 2009 à juillet 2012 :</strong> CAP Cuisine IFA Marcel Sauvage
+                        Mont-Saint-Aignan, France</p>
+                    <p><strong>De juillet 2009 à juillet 2012 :</strong> Apprentissage Cuisine La P'tite Fontaine
+                        Mont-Saint-Aignan,
+                        France</p>
+                </div>
+            </section>
+            <section>
+                <div class="content">
+                    <header>
+                        <a href="#" class="icon fa-line-chart"><span class="label">Icon</span></a>
+                        <h3><strong>Expériences professionnelles</strong></h3>
+                    </header>
+                    <p><strong>Avril 2017 :</strong> Commis de cuisine Midi Vin Mont-Saint-Aignan, France</p>
+                    <p><strong>Mars 2017 :</strong> Stagiaire Biotropica, les jardinds animaliers Val-de-Reuil, France
+                    </p>
+                    <p><strong>Avril 2015 :</strong> Commis de cuisine Eden Hôtel Bois-Guillaume, France</p>
+                    <p><strong>Juin 2013 :</strong> Commis de cuisine La P'tite Fontaine Mont-Saint-Aignan, France</p>
+                    <p><strong>D'août 2012 à mars 2013 :</strong> Commis de cuisine La P'tite Fontaine
+                        Mont-Saint-Aignan, France</p>
+                    <p><strong>De juillet 2011 à août 2011 :</strong> Commis de cuisine La P'tite Fontaine
+                        Mont-Saint-Aignan, France</p>
+                </div>
+            </section>
+        </div>
+    </div>
+</section>
+
+<!-- CTA -->
+<section id="cta" class="wrapper">
+    <div class="inner">
+        <h2>Mon parcours</h2>
+        <p>J'ai commencé mon CAP Cuisine à 15 ans au CFA Marcel Sauvage à Mont-Saint-Aignan où j'étais en alternance
+            dans le restaurant traditionnel la P'tite Fontaine qui se situais lui aussi à Mont-Saint-Aignan.</p>
+
+        <p>Malgré de nombreux problèmes de santé qui m'ont conduit à faire une 3ème année du CAP, j'ai validé mon
+            diplôme professionnel. J'ai ensuite travaillé en tant que commis de cuisine pour la P'tite Fontaine.
+            &Agrave; l'annonce du départ de mon chef qui était également mon maitre d'apprentissage, je devais prendre
+            ça place mais suite à des différends avec mon employeur je suis donc partis moi aussi.</p>
+
+        <p>Après mon départ, j'ai décidé de faire une pause sabbatique pour réfléchir à mon avenir professionnel, à
+            savoir si je continuais dans la cuisine ou non.
+            J'ai choisi de continuer dans ce domaine-ci, j'ai trouvé un poste de commis de cuisine dans un Hôtel,
+            malheureusement l'expérience fut désastreuse, mes problèmes de santé se sont déclaré à nouveau....</p>
+
+        <p>Finalement, j'ai été recconus travailleur handicapé et d'après l'avis de nombreux médecins que j'ai
+            consultés, j'ai dû me décider à arrêter complètement la cuisine à cause de mon état qui n'était plus
+            compatible avec ce travail. S'en suit une recherche de réorientation, qui pourraient être compatible avec
+            mes soucis de santé. J'ai eu la chance d'effectuer un stage dans le parc animalier de Biootropica, qui c'est
+            très bien passer. Les soigneurs m'ont parlé d'une formation de soigneur animalier d'une durée de 9 mois et
+            cela ma fortement intéresser.
+            Malheureusement, je n'ai pas réussi à trouver un financement pour celle-ci....</p>
+
+        <p>Aujourd'hui j'ai décidé de m'orienter dans le domaine de l'informatique et du numérique, j'ai rejoint la
+            formation de "Référent Digital" où nous assistons à des cours sur le développement web et le marketing
+            digital, à la NFactory School au campus Saint-Marc de Rouen.</p>
+    </div>
+</section>
+
+<!-- Testimonials -->
+<section class="wrapper">
+    <div class="inner">
+        <header class="special">
+            <h2>Témoigniages</h2>
+            <p></p>
+        </header>
+        <div class="testimonials">
+            <section>
+                <div class="content">
+                    <blockquote>
+                        <p>Le meilleur étudiant que j'ai eut et de loin, donne envie de venir travailler et d'amener
+                            pleins de chouquettes de Bolbec.</p>
+                    </blockquote>
+                    <div class="author">
+                        <div class="image">
+                            <img src="images/picFred.jpg" alt=""/>
+                        </div>
+                        <p class="credit">- <strong>Frédéric Noël</strong> <span>Intervenant - NFactory School.</span>
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div class="content">
+                    <blockquote>
+                        <p>Un garçon surprenant, je le considérais plus comme mon second qu'un apprentis. Il était
+                            capable de gérer la cuisine en mon absence, et même me remplacer en cas de besoin.</p>
+                    </blockquote>
+                    <div class="author">
+                        <div class="image">
+                            <img src="images/pic03.jpg" alt=""/>
+                        </div>
+                        <p class="credit">- <strong>Ludovic</strong>
+                            <span>Chef et Maître d'apprentissage - La P'tite Fontaine.</span>
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div class="content">
+                    <blockquote>
+                        <p>Une source d'inspiration, il ma grandement aider pour notre projet fil rouge, tout le travail
+                            viens de lui.</p>
+                    </blockquote>
+                    <div class="author">
+                        <div class="image">
+                            <img src="images/picHugo.jpg" alt=""/>
+                        </div>
+                        <p class="credit">- <strong>Hugo Fakir</strong> <span>Etudiant RFD - NFactory School.</span></p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</section>
